bda2fe523e45fd896c88ade8cacc8c0607e19e99
AnatoliiSoloviov <anatolsoloviov@gmail.com>
2024-07-18 23:50:58 +0200

    add footer.js

---
+++ assets/js/footer.js
@@ -0,0 +1,33 @@
+// footer.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  // HTML код футера
+  const footerHTML = `
+            <section class="footer">
+                <ul>
+                    <li class="service-item">
+                        <div class="service-content-box">
+                            <p class="footer-item-text">
+                                Copyright &copy 2024 Soloviov Anatolii. All rights reserved
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+        `;
+
+  const footerLocations = [
+    "footer-location-1",
+    "footer-location-2",
+    "footer-location-3",
+    "footer-location-4",
+
+  ];
+
+  footerLocations.forEach((locationId) => {
+    const targetElement = document.getElementById(locationId);
+    if (targetElement) {
+      targetElement.insertAdjacentHTML("beforeend", footerHTML);
+    }
+  });
+});
